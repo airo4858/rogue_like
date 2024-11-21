d8f1a3c5d833c6b30f0be8331c8347b41f69fa13
Aidan Rogers <143754636+airo4858@users.noreply.github.com>
2024-11-21 14:23:00 -0700

    added out of bounds

--- player.gd
+++ player.gd
@@ -8,26 +8,34 @@ var can_stab : bool = true
 
 func jump(event):
 	if can_jump == true:
+		var initial_player_position = self.position
 		can_jump = false
 		$AnimationPlayer.play("beta_jump")
 		await $AnimationPlayer.animation_finished
+		var overlap = $CollisionArea.get_overlapping_bodies()
+		if (not overlap.is_empty()):
+			self.position = initial_player_position
 		can_jump = true
 
 func sweep(event):
 	if can_sweep == true:
 		can_sweep = false
+		can_stab = false
 		get_node("Sweep").look_at(get_global_mouse_position())
 		$AnimationPlayer.play("sweep_attack")
 		await $AnimationPlayer.animation_finished
 		can_sweep = true
+		can_stab = true
 
 func stab(event):
 	if can_stab == true:
 		can_stab = false
+		can_sweep = false
 		get_node("Stab").look_at(get_global_mouse_position())
 		$AnimationPlayer.play("stab_attack")
 		await $AnimationPlayer.animation_finished
 		can_stab = true
+		can_sweep = true
 
 func _physics_process(delta):
 	if self.velocity == Vector2(0,1) * move_speed or self.velocity == Vector2(0,-1) * move_speed:
@@ -37,12 +45,12 @@ func _physics_process(delta):
 	
 	if (Input.is_action_pressed("jump")):
 		jump("jump")
-		print("JUMP!")
+		#print("JUMP!")
 		
 	if (Input.is_action_pressed("stab")):
 		stab("stab")
-		print("STAB")
+		#print("STAB")
 		
 	if (Input.is_action_pressed("sweep")):
 		sweep("sweep")
-		print("SWEEP")
+		#print("SWEEP")
