bafd5780cddc1631da235ac3278ec5f1ff3917e2
Aidan Rogers <143754636+airo4858@users.noreply.github.com>
2024-12-10 17:41:16 -0700

    added a not fully functioning 2nd enemy

--- enemy_2.gd
+++ enemy_2.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
-class_name Enemy2
+class_name Enemy
 
 signal healthChanged
 signal enemyDeath
 
-@export var max_hp: int = 10
-@export var hp: int = 10
+#Add a functioning shooting state
+
+@export var max_hp: int = 6
+@export var hp: int = 6
 
 func hit(damage_number: int):
 	hp -= damage_number
